57221e38c7bef2e100aa4a77127d566d659969d9
Karan <karan101099@gmail.com>
2020-08-31 02:55:18 +0530

    added a whole bunch of stuff

---
+++ portfolio/js/script.js
@@ -0,0 +1,8 @@
+function onClickProjects () {
+  var element = document.getElementById("projects");
+
+// element.scrollIntoView();
+// element.scrollIntoView(false);
+// element.scrollIntoView({block: "end"});
+element.scrollIntoView({behavior: "smooth"});
+}
